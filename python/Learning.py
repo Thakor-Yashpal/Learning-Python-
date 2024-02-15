62741742e8d9f74a2d884c695877bb3701d41f41
Th-Yash <thakoryashpal755@gmail.com>
2024-02-15 21:26:09 +0530

    Create Learning.py

---
+++ python/Learning.py
@@ -0,0 +1,15 @@
+ def My_function ():
+         new_patient = input('what is your Nmae :')
+         patient_age = int(input('Age:'))
+         new_one = True
+        
+         print(new_patient,patient_age,new_one)
+
+ My_function()
+
+list1 = ["apple", "orange", "banana","watermelon" ]
+
+list1[1] = "Nova" 
+list1[2] = 12
+
+print(list1)
